9a6c6ef5b82dfdbc85e3eabe00837cceff7ecff9
Aparnakrishnan-Vonnue <aparnakrishnan3920@gmail.com>
2023-05-20 21:35:12 +0530

    feat: program to return list of even integers from an integers list

---
+++ pg003-list-of-even-nummbers/script.js
@@ -0,0 +1,17 @@
+let listOfIntegersOne = [1, 2, 3, 4, 5, 6]
+let listOfIntegersTwo = [7, 8, 9, 10, 11, 12]
+let listOfIntegersThree = [2, 4, 6, 8, 10]
+
+function returnListOfEvenNum(array){
+  let listOfEvenNums = []
+  for(let i = 0; i < array.length; i++){
+    if(array[i] % 2 === 0){
+      listOfEvenNums.push(array[i])
+    }
+  }
+  return listOfEvenNums
+}
+
+console.log(returnListOfEvenNum(listOfIntegersOne))
+console.log(returnListOfEvenNum(listOfIntegersTwo))
+console.log(returnListOfEvenNum(listOfIntegersThree))
