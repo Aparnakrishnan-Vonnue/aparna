3465d05064fa3da6c0826253adb50129fcad6188
Aparna <aparna.k@vonnue.com>
2023-05-30 14:04:07 +0530

    feat: task to find the longest common prefix

--- pg010-common-prefix/script.js
+++ pg010-common-prefix/script.js
@@ -1,11 +1,18 @@
-let listOfStrings = ["flower", "flow", "flight"]
+let listOfStrings = ["apple", "app", "application"]
 
 function longestCommonPrefix(list){
   // debugger
+  let countOfCommonLetters = 0
   for(let i = 0; i < list.length; i++){
-   for(let j = 0; j < list[i].length; j++){
-    console.log(list[i][j])
-   }
+    if(list[i].length < list[i + 1].length){
+      for(let j = 0; j < list[i].length; j++){     
+       if(list[i][j] === list[i + 1][j]){
+         countOfCommonLetters += 1
+         console.log(countOfCommonLetters)
+         console.log(list[i][j])
+       }
+      }
+    }
   }
 }
 
