8caea4228e8f022f4ca50374f8127dc1241f2b31
Aparna <aparna.k@vonnue.com>
2023-06-02 15:19:03 +0530

    task to find the max product of three numbers

--- pg012/script.js
+++ pg012/script.js
@@ -1,16 +1,29 @@
-let listOfNum = [1, 2, 3, 4, 5]
+// let listOfNum = [1, 2, 3, 4, 5]
+// let listOfNum = [-8, -6, 4, 1, 10];
+let listOfNum = [9, 8, 2, -4, -5]
 
-function maximumProduct(){
-  // debugger
-  let greaterNum = listOfNum[0]
-  let maxNumArray = []
-  for(let i = 0; i < listOfNum.length; i++){
-    if(listOfNum[i] < listOfNum[i + 1]){
-      greaterNum = listOfNum[i + 1]
-        maxNumArray.push(greaterNum)
+function maximumProduct() {
+  let tempStorage = "";
+  for (let i = 0; i < listOfNum.length; i++) {
+    for (let j = i + 1; j < listOfNum.length; j++) {
+      if (listOfNum[i] < listOfNum[j]) {
+        tempStorage = listOfNum[i];
+        listOfNum[i] = listOfNum[j];
+        listOfNum[j] = tempStorage;
+      }
     }
   }
-  return maxNumArray
+  let product = listOfNum[0];
+ 
+  for (let k = 0; k < listOfNum.length; k++) {
+    let productOne = product * (listOfNum[1] * listOfNum[2])
+    let productTwo = product * (listOfNum[listOfNum.length - 1] * listOfNum[listOfNum.length - 2]) 
+    if(productOne > productTwo){
+      return productOne
+    }else{
+      return productTwo
+    }
+  }
 }
 
-console.log(maximumProduct(listOfNum))
+console.log(maximumProduct(listOfNum));
