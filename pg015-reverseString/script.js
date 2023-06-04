714ad8919330a18732102d2e1c3cd07e70465fdf
Aparna <aparna.k@vonnue.com>
2023-06-04 19:16:14 +0530

    task to find the reverse of words in sentence

---
+++ pg015-reverseString/script.js
@@ -0,0 +1,41 @@
+// function reverseWordsInAString(){
+  //   // debugger
+//   let inputArray = inputString.split(" ")
+//   let reversedArray = []
+//   for(let i = 0; i < inputArray.length; i++){
+  //     for(let j = inputArray[i].length; j > -1; j--){
+    //      reversedArray.push(inputArray[i][j])
+    //     }
+//   }
+//   for(let k = 0; k < reversedArray.length; k++){
+  //     if(reversedArray[k] === undefined){
+    //       reversedArray[k] = " "
+    //       console.log(reversedArray)
+    //     }
+    //   }
+    // }
+    
+    // console.log(reverseWordsInAString(inputString))
+    
+// let inputString = "Hello World "
+// let i = 0;
+// let pos = 0
+// let str = ""
+// let start = 0
+// let space = true
+// while(i < inputString.length){
+//   if(inputString[i] == " " || i == inputString.length-1){
+//     start = pos 
+//     space ? pos = i -1 : pos = i ; 
+//     while(pos >= start){
+//       str = str + inputString[pos]
+//       pos--
+//     } 
+//     space = false
+//     pos = i 
+//   }
+//   i++
+// }
+// console.log(str)
+
+
