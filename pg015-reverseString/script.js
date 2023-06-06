d1226c0b235e931171954805fc5836c3db3d1384
Aparna <aparna.k@vonnue.com>
2023-06-06 17:05:51 +0530

    task to find the reverse of words in sentence

--- pg015-reverseString/script.js
+++ pg015-reverseString/script.js
@@ -1,41 +1,21 @@
-// function reverseWordsInAString(){
-  //   // debugger
-//   let inputArray = inputString.split(" ")
-//   let reversedArray = []
-//   for(let i = 0; i < inputArray.length; i++){
-  //     for(let j = inputArray[i].length; j > -1; j--){
-    //      reversedArray.push(inputArray[i][j])
-    //     }
-//   }
-//   for(let k = 0; k < reversedArray.length; k++){
-  //     if(reversedArray[k] === undefined){
-    //       reversedArray[k] = " "
-    //       console.log(reversedArray)
-    //     }
-    //   }
-    // }
-    
-    // console.log(reverseWordsInAString(inputString))
-    
-// let inputString = "Hello World "
-// let i = 0;
-// let pos = 0
-// let str = ""
-// let start = 0
-// let space = true
-// while(i < inputString.length){
-//   if(inputString[i] == " " || i == inputString.length-1){
-//     start = pos 
-//     space ? pos = i -1 : pos = i ; 
-//     while(pos >= start){
-//       str = str + inputString[pos]
-//       pos--
-//     } 
-//     space = false
-//     pos = i 
-//   }
-//   i++
-// }
-// console.log(str)
+let inputString = "Hello World"
 
+function reverseString(){
+  // debugger
+  let indicator = 0
+  let initialPoint = 0
+  let reversedStr = ""
+  for(let i = 0; i < inputString.length; i++){
+    if(inputString[i] === " " || i === inputString.length - 1){
+      initialPoint = indicator
+      indicator = i
+      for(let j = indicator; j > initialPoint - 1; j--){
+        reversedStr += inputString[j]
+      }
+    }
+  }
+  return reversedStr
+ 
+}
 
+console.log(reverseString(inputString))
