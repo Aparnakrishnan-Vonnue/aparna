d31996a84a49b3dcc09a3dbf856feb7a95dcc762
Aparnakrishnan-Vonnue <aparnakrishnan3920@gmail.com>
2023-06-19 21:35:56 +0530

    task to validate email

--- pg009-validEmail/script.js
+++ pg009-validEmail/script.js
@@ -2,7 +2,9 @@ let domains = ["com", "net", "int", "org", "in"]
 let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 function validateEmail(){
- 
+ for(let i = 0; i < domains.length; i++){
+  console.log(domains)
+ }
 }
 
 console.log(validateEmail)
