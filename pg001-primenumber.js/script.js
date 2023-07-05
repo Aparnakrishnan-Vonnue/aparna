7f1f916ffec48ce36cccfb1678c14f090006b626
Aparnakrishnan-Vonnue <aparnakrishnan3920@gmail.com>
2023-07-05 15:35:08 +0530

    add: test case added to isPrime function

--- pg001-primenumber.js/script.js
+++ pg001-primenumber.js/script.js
@@ -1,23 +1,68 @@
-
 function isPrime(number) {
-  //identify if the number is a float
   if (number === 1) {
     return `${number} is neither prime nor composite`;
   }
 
-  if(number < 1)return false
+  if (number < 1 || Math.round(number) !== number || typeof(number) === "string") return false;
 
   let i = 2;
-  while (i < number) {
+  while (i < number/2) {
     if (number % i === 0) {
-      console.log(`${number} is not a prime number`)
       return false;
     }
     i++;
   }
-  console.log(`${number} is a prime number`)
-  return true;
 
+  return true;
 }
 
-console.log(isPrime(2));
+const testCases = [
+  {
+    value: 1,
+    expected: "1 is neither prime nor composite",
+  },
+  {
+    value: 2,
+    expected: true,
+  },
+  {
+    value: -1,
+    expected: false,
+  },
+  {
+    value: 0,
+    expected: false,
+  },
+  {
+    value: 2.5,
+    expected: false,
+  },
+  {
+    value: 2.0,
+    expected: true,
+  },
+  {
+    value: "aparna",
+    expected: false,
+  },
+];
+
+const testIsPrime = (tcases, i) => {
+  tcases.forEach((tcase, i) => {
+    if (isPrime(tcase.value) === tcase.expected) {
+      console.log("test case " + i + " passed");
+    } else {
+      console.log(
+        "test case " +
+          i +
+          " failed." +
+          " expected value: " +
+          tcase.expected +
+          " got: " +
+          isPrime(tcase.value)
+      );
+    }
+  });
+};
+
+testIsPrime(testCases);
