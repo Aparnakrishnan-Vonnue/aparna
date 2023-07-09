322dbecd3355f268b8c4cbc23bad541cc7764654
Aparnakrishnan-Vonnue <aparnakrishnan3920@gmail.com>
2023-07-09 22:22:50 +0530

    Update script.js

--- pg003-list-of-even-nummbers/script.js
+++ pg003-list-of-even-nummbers/script.js
@@ -2,7 +2,7 @@ let listOfIntegersOne = [1, 2, 3, 4, 5, 6]
 let listOfIntegersTwo = [7, 8, 9, 10, 11, 12]
 let listOfIntegersThree = [2, 4, 6, 8, 10]
 
-function returnListOfEvenNum(array){
+function ListOfEvenNum(array){
   let listOfEvenNums = []
   for(let i = 0; i < array.length; i++){
     if(array[i] % 2 === 0){
@@ -12,5 +12,12 @@ function returnListOfEvenNum(array){
   return listOfEvenNums
 }
 
+
 console.log(returnListOfEvenNum(listOfIntegersOne))
 console.log(returnListOfEvenNum(listOfIntegersTwo))
+
+
+
+function testListOfEvenNum(){
+  
+}
