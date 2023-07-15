da3338cf69e718ef5aa3aaa7d9527b103d0c8367
Aparnakrishnan-Vonnue <aparnakrishnan3920@gmail.com>
2023-07-15 20:13:53 +0530

    feat:testcases for algorithmic tasks

--- pg002-wordCount.js/script.js
+++ pg002-wordCount.js/script.js
@@ -1,28 +1,61 @@
-let sentenceOne = "This is a sample sentence"
-let sentenceTwo = "Hello World"
-let sentenceThree = "Programming is fun"
-let sentenceFour = "Hello"
 
-function returnCountOfWords(sentence){
+
+function count_of_words(sentence){
   let i = 0
   let count = 0
+  
   while (i < sentence.length){
-    if(sentence[i] === " "){
+
+    if(sentence[i] === " " && i !== sentence.length - 1 && sentence[i + 1] !== " "){
       count = count + 1
     }
-    if(i === sentence.length - 1){
+
+    if(i === sentence.length - 1 ){
       count = count + 1
       return count
-    }
-    if(i === sentence.length && sentence[i] !== ""){
-      return 1
     }
     i++ 
   }
 }
 
-console.log(returnCountOfWords(sentenceOne))
-console.log(returnCountOfWords(sentenceTwo))
-console.log(returnCountOfWords(sentenceThree))
-console.log(returnCountOfWords(sentenceFour))
 
+
+
+const testCases = [
+  {
+    sentence: "Hello",
+    expected: 1
+  },
+  {
+    sentence: "Programming is fun.",
+    expected: 3
+  },
+  {
+    sentence: "This is a sample sentence.",
+    expected: 5
+  },
+  {
+    sentence: "1",
+    expected: 1
+  },
+  {
+    sentence: "good ",
+    expected: 1
+  },
+  {
+    sentence: "happy  Birthday.",
+    expected: 2
+  }
+]
+
+function test_count_of_words(tcases){
+ tcases.forEach((tcase, idx) => {
+  if(count_of_words(tcase.sentence) === tcase.expected){
+    console.log("Testcase " + idx + " passed")
+  }else {{
+    console.log("Testcase " + idx + " failed. "+"expected: "+ tcase.expected + " got: " + count_of_words(tcase.sentence))
+  }}
+ })
+}
+
+test_count_of_words(testCases)
