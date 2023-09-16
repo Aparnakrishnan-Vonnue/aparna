812a71182c6ce092e0c19c694682a0263ebcde49
Aparna Krishnan <aparna.k@vonnue.com>
2023-09-16 18:32:46 +0530

    update function to find convergent series max length

--- series/script.js
+++ series/script.js
@@ -1,57 +1,59 @@
-function longestSequenceBtwTwoNumbers(num1, num2) {
-  let mainArray = [];
-  let sequence = [num1];
-  let setOfSequences = [];
-  let resultantArray = [];
-  for (let i = num1; i <= num2; i++) {
-    mainArray.push(i);
-  }
-  for (let start = 0; start < mainArray.length; start++) {
-    if (num1 === 0) {
-      return `this will result in an infinite loop`;
-    }
-    if (sequence.length === 0) {
-      sequence.push(mainArray[start]);
-    }
-    for (let j = 0; j <= sequence.length; j++) {
-      let inputNum = sequence[j];
-      if (inputNum % 2 === 0) {
-        sequence.push(inputNum / 2);
-      } else {
-        sequence.push(3 * inputNum + 1);
-      }
-      if (sequence[sequence.length - 1] === 1) {
-        setOfSequences.push(sequence);
-        sequence = [];
-        break;
-      }
-    }
-  }
-  let longest = setOfSequences[0].length;
-  setOfSequences.forEach((sequence) => {
-    if (sequence.length > longest) {
-      longest = sequence.length;
-    }
-  });
-  resultantArray.push(num1, num2, longest);
-  return resultantArray;
-}
+// function convergentSeriesMaxLength(num1, num2) {
+//   let sequence = [num1];
+//   let numOfIteration = num1;
+//   let lengthArray = [];
+
+//   while (numOfIteration <= num2) {
+//     if (num1 === 0) {
+//       return `this will result in an infinite loop`;
+//     }
+//     if (!sequence.length) {
+//       sequence.push(numOfIteration);
+//     }
+//     for (let j = 0; j <= sequence.length; j++) {
+//       let inputNum = sequence[j];
+//       if (inputNum % 2 === 0) {
+//         sequence.push(inputNum / 2);
+//       } else {
+//         sequence.push(3 * inputNum + 1);
+//       }
+//       if (sequence[sequence.length - 1] === 1) {
+//         lengthArray.push(sequence.length);
+//         sequence = [];
+//         break;
+//       }
+//     }
+//     numOfIteration++;
+//   }
+
+//   let longest = lengthArray[0];
+
+//   lengthArray.forEach((length) => {
+//     if (length > longest) {
+//       longest = length;
+//     }
+//   });
+
+//   return `The longest convergent series for ${num1}, ${num2} is ${longest}`;
+// }
+
+// console.log(convergentSeriesMaxLength(100000, 10000000))
 
 const testCases = [
   {
     num1: 2,
     num2: 3,
-    expectedOutput: [2, 3, 8],
+    expectedOutput: `The longest convergent series for ${2}, ${3} is ${8}`,
   },
   {
     num1: 1,
     num2: 5,
-    expectedOutput: [1, 5, 8],
+    expectedOutput: `The longest convergent series for ${1}, ${5} is ${8}`,
   },
   {
     num1: 2,
     num2: 6,
-    expectedOutput: [2, 6, 9],
+    expectedOutput: `The longest convergent series for ${2}, ${6} is ${9}`,
   },
   {
     num1: 0,
@@ -61,37 +63,69 @@ const testCases = [
   {
     num1: 1,
     num2: 10,
-    expectedOutput: [1, 10, 20],
+    expectedOutput: `The longest convergent series for ${1}, ${10} is ${20}`,
   },
   {
     num1: 5,
     num2: 5,
-    expectedOutput: [5, 5, 6],
+    expectedOutput: `The longest convergent series for ${5}, ${5} is ${6}`,
+  },
+  {
+    num1: 1,
+    num2: 10000000,
+    expectedOutput: `The longest convergent series for ${1}, ${10000000} is ${686}`,
   },
 ];
 
 function testLongestChainBtwTwoNumbers(tCases) {
   tCases.forEach((tCase, idx) => {
-    for (
-      let i = 0, j = 0;
-      i < tCase.expectedOutput.length,
-        j < longestSequenceBtwTwoNumbers(tCase.num1, tCase.num2).length;
-      i++, j++
+    if (
+      tCase.expectedOutput === convergentSeriesMaxLength(tCase.num1, tCase.num2)
     ) {
-      if (
-        tCase.expectedOutput[i] ===
-        longestSequenceBtwTwoNumbers(tCase.num1, tCase.num2)[j]
-      ) {
-        console.log(`Testcase for ${idx} passed`);
-      } else {
-        console.log(
-          `Testcase for ${idx} failed. Expected output was ${
-            tCase.expectedOutput
-          } but got ${longestSequenceBtwTwoNumbers(tCase.num1, tCase.num2)}`
-        );
-      }
+      console.log(
+        `Testcase for ${idx} passed. The out put is: ${tCase.expectedOutput}`
+      );
+    } else {
+      console.log(
+        `Testcase for ${idx} failed. The expected output was ${
+          tCase.expectedOutput
+        }, but got ${convergentSeriesMaxLength(tCase.num1, tCase.num2)}`
+      );
     }
   });
 }
 
-testLongestChainBtwTwoNumbers(testCases);
+// testLongestChainBtwTwoNumbers(testCases);
+
+function convergentSeriesMaxLength(num1, num2) {
+  let sequence = [num1];
+  let numOfIteration = num1;
+  let length = 0;
+
+  while (numOfIteration <= num2) {
+    if (num1 === 0) {
+      return `this will result in an infinite loop`;
+    }
+    if (!sequence.length) {
+      sequence.push(numOfIteration);
+    }
+    for (let j = 0; j <= sequence.length; j++) {
+      let inputNum = sequence[j];
+      inputNum % 2 === 0
+        ? sequence.push(inputNum / 2)
+        : sequence.push(3 * inputNum + 1);
+      if (sequence[sequence.length - 1] === 1) {
+        if (sequence.length > length) {
+          length = sequence.length;
+          sequence = [];
+        }
+        sequence = [];
+        break;
+      }
+    }
+    numOfIteration++;
+  }
+  return `The longest convergent series for ${num1}, ${num2} is ${length}`;
+}
+
+console.log(convergentSeriesMaxLength(2, 10000000));
