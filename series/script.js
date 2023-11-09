ca75f0e583976390079280ef2a75a3359bc05468
Aparna Krishnan <aparna.k@vonnue.com>
2023-11-09 08:46:00 +0530

    memoization of convergent series

--- series/script.js
+++ series/script.js
@@ -72,62 +72,6 @@
 //   return `The longest convergent series for ${num1}, ${num2} is ${length}`;
 // }
 
-const testCases = [
-  {
-    num1: 2,
-    num2: 3,
-    expectedOutput: `The longest convergent series for ${2}, ${3} is ${8}`,
-  },
-  {
-    num1: 1,
-    num2: 5,
-    expectedOutput: `The longest convergent series for ${1}, ${5} is ${8}`,
-  },
-  {
-    num1: 2,
-    num2: 6,
-    expectedOutput: `The longest convergent series for ${2}, ${6} is ${9}`,
-  },
-  {
-    num1: 0,
-    num2: 1,
-    expectedOutput: "this will result in an infinite loop",
-  },
-  {
-    num1: 1,
-    num2: 10,
-    expectedOutput: `The longest convergent series for ${1}, ${10} is ${20}`,
-  },
-  {
-    num1: 5,
-    num2: 5,
-    expectedOutput: `The longest convergent series for ${5}, ${5} is ${6}`,
-  },
-  {
-    num1: 1,
-    num2: 10000000,
-    expectedOutput: `The longest convergent series for ${1}, ${10000000} is ${686}`,
-  },
-];
-
-function testLongestChainBtwTwoNumbers(tCases) {
-  tCases.forEach((tCase, idx) => {
-    if (
-      tCase.expectedOutput === convergentSeriesMaxLength(tCase.num1, tCase.num2)
-    ) {
-      console.log(
-        `Testcase for ${idx} passed. The out put is: ${tCase.expectedOutput}`
-      );
-    } else {
-      console.log(
-        `Testcase for ${idx} failed. The expected output was ${
-          tCase.expectedOutput
-        }, but got ${convergentSeriesMaxLength(tCase.num1, tCase.num2)}`
-      );
-    }
-  });
-}
-
 // function convergentSeriesMaxLength(num1, num2) {
 //   let sequence = [num1];//[10]
 //   let numOfIteration = num1;//10
@@ -175,48 +119,183 @@ function testLongestChainBtwTwoNumbers(tCases) {
 
 // console.log(convergentSeriesMaxLength(1, 100000));
 
-function convergentSeriesMaxLength(num1, num2) {
-  let sequence = [num1];
-  let numOfIteration = num1;
-  let length = 0;
-  let dictionary = [];
+// function convergentSeriesMaxLength(num1, num2) {
+//   let sequence = [num1];
+//   let numOfIteration = num1;
+//   let length = 0;
+//   let dictionary = [];
 
-  while (numOfIteration <= num2) {
-    if (num1 === 0) {
-      return `this will result in an infinite loop`;
-    }
-    if (!sequence.length) {
-      sequence.push(numOfIteration);
-    }
-    for (let j = 0; j <= sequence.length; j++) {
-      let inputNum = sequence[j];
-      inputNum % 2 === 0
-        ? sequence.push(inputNum / 2)
-        : sequence.push(3 * inputNum + 1);
-      if (sequence[sequence.length - 1] === 1) {
-        if (sequence.length > length) {
-          length = sequence.length;
-          for (let i = 0; i < sequence.length; i++) {
-            for(let n = 0; n < dictionary.length; n++){
-              if(dictionary[n] === sequence[i]){
+//   while (numOfIteration <= num2) {
+//     if (num1 === 0) {
+//       return `this will result in an infinite loop`;
+//     }
+//     if (!sequence.length) {
+//       sequence.push(numOfIteration);
+//     }
+//     for (let j = 0; j <= sequence.length; j++) {
+//       let inputNum = sequence[j];
+//       dictionary.forEach((obj) => {
+//         Object.entries(obj).map(([key, value]) => {
+//           if (key === inputNum) {
+//             return;
+//           }
+//         });
+//       });
+//       inputNum % 2 === 0
+//         ? sequence.push(inputNum / 2)
+//         : sequence.push(3 * inputNum + 1);
+//       if (sequence[sequence.length - 1] === 1) {
+//         if (sequence.length > length) {
+//           length = sequence.length;
+//           for (let i = 0; i < sequence.length; i++) {
+//             dictionary.push({
+//               [sequence[i]]: sequence.length - i,
+//             });
+//           }
+//           sequence = [];
+//         }
+//         sequence = [];
+//         break;
+//       }
+//     }
+//     numOfIteration++;
+//   }
+//   // return dictionary
+//   return `The longest convergent series for ${num1}, ${num2} is ${length}`;
+// }
 
-                
-              }
-            }
-            dictionary.push({
-              [sequence[i]]: sequence.length - i,
-            });
-          }
-          sequence = [];
-        }
-        sequence = [];
-        break;
-      }
-    }
-    numOfIteration++;
+// console.log(convergentSeriesMaxLength(10, 40));
+
+// function convergentSeriesMaxLength(num1, num2) {
+//   let maxSeqLength = 0;
+//   let maxSeqStartingNum = 0;
+//   let dictionary = new Map();
+
+//   for (let i = num1; i <= num2; i++) {
+//     let sequence = [i];
+//     let currentNum = i;
+
+//     while (currentNum !== 1) {
+//       if (dictionary.has(currentNum)) {
+//         let stepsFromDictionary = dictionary.get(currentNum);
+//         for (let j = 0; j < sequence.length - 1; j++) {
+//           dictionary.set(
+//             sequence[j],
+//             stepsFromDictionary + sequence.length - j
+//           );
+//         }
+//         break;
+//       }
+//       if (currentNum % 2 === 0) {
+//         currentNum = currentNum / 2;
+//       } else {
+//         currentNum = 3 * currentNum + 1;
+//       }
+//       sequence.push(currentNum);
+//     }
+
+//     for (let j = 0; j < sequence.length; j++) {
+//       if (dictionary.has(sequence[j])) {
+//         break;
+//       }
+//       dictionary.set(sequence[j], sequence.length - j);
+//     }
+
+//     if (sequence.length > maxSeqLength) {
+//       maxSeqLength = sequence.length;
+//       maxSeqStartingNum = i;
+//     }
+//   }
+//   // console.log(dictionary);
+//   // return dictionary
+//   return `The longest convergent seriees for ${num1}, ${num2} starts with ${maxSeqStartingNum} and has a length of ${maxSeqLength}`;
+// }
+
+function getSequenceLength(num, dictionary) {
+  if (dictionary[num]) {
+    return dictionary[num];
   }
-  console.log(dictionary)
-  return `The longest convergent series for ${num1}, ${num2} is ${length}`;
+  let length = 1;
+  let originalNum = num;
+  while (num !== 1) {
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num = 3 * num + 1;
+    }
+    if (dictionary[num]) {
+      length += dictionary[num];
+      break;
+    }
+    length++;
+  }
+  dictionary[originalNum] = length;
+  return length;
 }
 
-console.log(convergentSeriesMaxLength(10, 40));
+function convergentSeriesMaxLength(num1, num2) {
+  let maxLength = 0;
+  const dictionary = {};
+
+  for (let i = num1; i <= num2; i++) {
+    const length = getSequenceLength(i, dictionary);
+    if (length > maxLength) {
+      maxLength = length;
+    }
+  }
+
+  return maxLength;
+}
+
+const testCases = [
+  {
+    num1: 2,
+    num2: 3,
+    expectedOutput: 8,
+  },
+  {
+    num1: 1,
+    num2: 5,
+    expectedOutput: 8,
+  },
+  {
+    num1: 2,
+    num2: 6,
+    expectedOutput: 9,
+  },
+  {
+    num1: 1,
+    num2: 10,
+    expectedOutput: 20,
+  },
+  {
+    num1: 5,
+    num2: 5,
+    expectedOutput: 6,
+  },
+  {
+    num1: 1,
+    num2: 10000000,
+    expectedOutput: 686,
+  },
+];
+
+function testLongestChainBtwTwoNumbers(tCases) {
+  tCases.forEach((tCase, idx) => {
+    if (
+      tCase.expectedOutput === convergentSeriesMaxLength(tCase.num1, tCase.num2)
+    ) {
+      console.log(
+        `Testcase for ${idx} passed. The out put is: ${tCase.expectedOutput}`
+      );
+    } else {
+      console.log(
+        `Testcase for ${idx} failed. The expected output was ${
+          tCase.expectedOutput
+        }, but got ${convergentSeriesMaxLength(tCase.num1, tCase.num2)}`
+      );
+    }
+  });
+}
+
+console.log(testLongestChainBtwTwoNumbers(testCases));
