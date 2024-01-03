f2cb657b7562f20b064a3b113fba83440ade7ba5
Aparnakrishnan-Vonnue <aparnakrishnan3920@gmail.com>
2024-01-03 08:58:32 +0530

    capitalize a string

--- pg026-randomArray/capitalize.js
+++ pg026-randomArray/capitalize.js
@@ -1,14 +1,41 @@
-function changeToTitleCase(sentence){
-    let newSentence = ""
-    for(let i = 0; i < sentence.length; i++){
-        if(sentence[i] === " "){
-           newSentence +=  sentence.charAt(i + 1).toUpperCase() + sentence.slice(i+2)
-        }else if(i === 0){
-            newSentence +=  sentence.charAt(i).toUpperCase() + sentence.slice(1)
-        }
-        sentence = newSentence
-        newSentence = ""
-    }
-    return sentence
+function changeToTitleCase(sentence) {
+  let words = sentence.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] =
+      words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+  }
+  return words.join(" ");
 }
-console.log(changeToTitleCase("hey prabhu, yeh kya hua?"))
+
+// console.log(changeToTitleCase("hey prabhu, yeh kya hua?"));
+
+const testObj = [
+  {
+    string: "hey prabhu, yeh kya hua?",
+    expectedOutput: "Hey Prabhu, Yeh Kya Hua?",
+  },
+  {
+    string: "dance",
+    expectedOutput: "Dance",
+  },
+  {
+    string: "run Across the sea",
+    expectedOutput: "Run Across The Sea",
+  },
+];
+
+const testcase = (testObj) => {
+  testObj.forEach((item, i) => {
+    if (item.expectedOutput === changeToTitleCase(item.string)) {
+      console.log(`Testcase passed for ${i}. The output is ${changeToTitleCase(item.string)}`);
+    } else {
+      console.log(
+        `Test case failed, Expected output was ${
+          item.expectedOutput
+        }, but got ${changeToTitleCase(item.string)}`
+      );
+    }
+  });
+};
+
+testcase(testObj);
