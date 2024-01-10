166fb224aaa221b5705cfa66d01b5d9b6bc75647
Aparna Krishnan <aparna.k@vonnue.com>
2024-01-10 11:50:51 +0530

    list and linkedlist

---
+++ programmes/list.js
@@ -0,0 +1,7 @@
+const list = [
+  { date: "12/1/2024", reading: 3, id: 20044 },
+  { date: "13/1/2024", reading: 5, id: 20045 },
+  { date: "14/1/2024", reading: 7, id: 20046 },
+];
+
+console.log(list[1].date)
